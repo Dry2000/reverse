3163ad79d60beb75e2487b994fbfcf485a75eb14
Dry <e1625@s.akashi.ac.jp>
2019-03-03 00:03:00 +0900

    Update Mov.swift

--- reverse/reverse/Mov.swift
+++ reverse/reverse/Mov.swift
@@ -7,6 +7,7 @@
 import Foundation
 enum line:Int{
     case BackWard = -1,Hold,Forword
+    static let allValues:[line]=[.BackWard,.Hold,.Forword]
 }
 typealias Direction = (vertical:line,horizonal:line)
 class Move{
@@ -15,9 +16,22 @@ class Move{
     let column:Int
     
     init(color:CellState,row:Int,column:Int){
-        self.color=color
-        self.row=row
-        self.column=column
+        self.color = color
+        self.row = row
+        self.column = column
     }
-    func count
+    func countFlippabeDisks(direction:Direction,cells:board<CellState>)->Int{//ひっくり返せる石の数を返り値として出力する
+        let y = direction.vertical.rawValue
+        let x = direction.horizonal.rawValue
+        var opponent = self.color.opponent
+        var count = 1
+        while(cells[self.row+count*y,self.column+count*x] == opponent){
+            count += 1
+        }
+        if(cells[self.row+count*y,self.column+count*x] == self.color){
+            return count-1
+        }else{
+            return 0
+        }
+    }
 }
