0b0cb62757bc4eed75f1c632193b2c6b8ba184cd
Dry <e1625@s.akashi.ac.jp>
2019-03-04 01:14:41 +0900

    Update GameScene.swift

--- reverse/reverse/GameScene.swift
+++ reverse/reverse/GameScene.swift
@@ -7,32 +7,31 @@
 import SpriteKit
 import GameplayKit
 
+
+let SquareHeight:CGFloat = 38.0
+let SquareWidth:CGFloat = 38.0
+let StoneImageNames = [
+    CellState.Black : "black",
+    CellState.White : "white",
+]
 class GameScene: SKScene {
-    
-    private var label : SKLabelNode?
-    private var spinnyNode : SKShapeNode?
-    
+    let gameLayer = SKNode()
+    let stoneLayer = SKNode()
+    var diskNodes = board<SKSpriteNode>(rows: BoardSize, columns: BoardSize)
+    var boardState:Board!
     override func didMove(to view: SKView) {
+        super.anchorPoint = CGPoint(x:0.5,y:0.5)
         
-        // Get label node from scene and store it for use later
-        self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
-        if let label = self.label {
-            label.alpha = 0.0
-            label.run(SKAction.fadeIn(withDuration: 2.0))
-        }
-        
-        // Create shape node to use during mouse interaction
-        let w = (self.size.width + self.size.height) * 0.05
-        self.spinnyNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.3)
-        
-        if let spinnyNode = self.spinnyNode {
-            spinnyNode.lineWidth = 2.5
-            
-            spinnyNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(Double.pi), duration: 1)))
-            spinnyNode.run(SKAction.sequence([SKAction.wait(forDuration: 0.5),
-                                              SKAction.fadeOut(withDuration: 0.5),
-                                              SKAction.removeFromParent()]))
-        }
+        let background=SKSpriteNode(imageNamed:"background")
+        background.zPosition = -1
+        background.size = self.size
+        self.addChild(background)
+        let layerPosition = CGPoint(x:-SquareWidth*CGFloat(BoardSize),y:-SquareHeight*CGFloat(BoardSize))
+        self.stoneLayer.position = layerPosition
+        self.gameLayer.addChild(self.stoneLayer)
+       /* let test = SKSpriteNode(imageNamed:"black")
+        print(test.position)
+        self.stoneLayer.addChild(test)*/
     }
     
     
@@ -41,7 +40,10 @@ class GameScene: SKScene {
     }
     
     override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-    
+        for touch:AnyObject in touches{
+            let location=touch.location(in:self)
+            print(location)
+        }
     }
     
     override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
@@ -55,5 +57,38 @@ class GameScene: SKScene {
     
     override func update(_ currentTime: TimeInterval) {
         
+    }
+    func initBoard(){
+        self.boardState = Board()
+        self.updateDiskNodes()
+    }
+    func updateDiskNodes(){
+        for row in 0..<BoardSize{
+            for column in 0..<BoardSize{
+                if let state = self.boardState.cells[row,column]{
+                    if let imageName = StoneImageNames[state]{
+                        if let prevNode = self.diskNodes[row,column]{
+                            if prevNode.userData?["state"] as! Int == state.rawValue{
+                                continue
+                            }
+                            prevNode.removeFromParent()
+                        }
+                        let newNode = SKSpriteNode(imageNamed: imageName)
+                        newNode.userData = ["state":state.rawValue] as NSMutableDictionary
+                        newNode.xScale = SquareWidth/newNode.size.width
+                        newNode.yScale = SquareHeight/newNode.size.height
+                        newNode.position = self.convertPointOnLayer(row: row,column:column)
+                        newNode.zPosition = 10000
+                        self.stoneLayer.addChild(newNode)
+                        print("\(newNode.position),\(newNode.zPosition) \n")
+                        self.diskNodes[row,column] = newNode
+                        
+                    }
+                }
+            }
+        }
+    }
+    func convertPointOnLayer(row:Int,column:Int)->CGPoint{
+        return CGPoint(x:CGFloat(column)*SquareWidth+SquareWidth/2,y:CGFloat(row)*SquareHeight+SquareHeight/2)
     }
 }
