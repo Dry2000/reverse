c9c112884f415df374aa9a903aa3556c90e72c36
Dry <e1625@s.akashi.ac.jp>
2019-03-04 23:37:36 +0900

    Update GameScene.swift
    近年稀に見るゴリ押し
    参考にしない方が良い（誰もしない

--- reverse/reverse/GameScene.swift
+++ reverse/reverse/GameScene.swift
@@ -7,9 +7,10 @@
 import SpriteKit
 import GameplayKit
 
+var positions:[[CGPoint]]!
 
-let SquareHeight:CGFloat = 38.0
-let SquareWidth:CGFloat = 38.0
+let SquareHeight:CGFloat = 20.0
+let SquareWidth:CGFloat = 20.0
 let StoneImageNames = [
     CellState.Black : "black",
     CellState.White : "white",
@@ -29,21 +30,27 @@ class GameScene: SKScene {
         let layerPosition = CGPoint(x:-SquareWidth*CGFloat(BoardSize),y:-SquareHeight*CGFloat(BoardSize))
         self.stoneLayer.position = layerPosition
         self.gameLayer.addChild(self.stoneLayer)
-       /* let test = SKSpriteNode(imageNamed:"black")
-        print(test.position)
-        self.stoneLayer.addChild(test)*/
+        let test = SKSpriteNode(imageNamed:"enemy")
+        print(stoneLayer.position ,stoneLayer.zPosition)
+        //print(test.position)
+      //  self.addChild(test)
     }
     
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-    
+        for touch:AnyObject in touches{
+            let location=touch.location(in:self)
+            if let (row,column) = self.convertPointOnBoard(point:location){
+                print(row,column)
+            }
+        }
     }
     
     override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for touch:AnyObject in touches{
+      /*  for touch:AnyObject in touches{
             let location=touch.location(in:self)
             print(location)
-        }
+        }*/
     }
     
     override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
@@ -75,12 +82,11 @@ class GameScene: SKScene {
                         }
                         let newNode = SKSpriteNode(imageNamed: imageName)
                         newNode.userData = ["state":state.rawValue] as NSMutableDictionary
-                        newNode.xScale = SquareWidth/newNode.size.width
-                        newNode.yScale = SquareHeight/newNode.size.height
+                        newNode.size = CGSize(width: SquareWidth*5, height: SquareHeight*5)
                         newNode.position = self.convertPointOnLayer(row: row,column:column)
-                        newNode.zPosition = 10000
-                        self.stoneLayer.addChild(newNode)
-                        print("\(newNode.position),\(newNode.zPosition) \n")
+                       // newNode.zPosition = 10000
+                        self.addChild(newNode)
+                       // print("\(newNode.position),\(newNode.zPosition) \n")
                         self.diskNodes[row,column] = newNode
                         
                     }
@@ -89,6 +95,51 @@ class GameScene: SKScene {
         }
     }
     func convertPointOnLayer(row:Int,column:Int)->CGPoint{
-        return CGPoint(x:CGFloat(column)*SquareWidth+SquareWidth/2,y:CGFloat(row)*SquareHeight+SquareHeight/2)
+        var x:CGFloat
+        var y:CGFloat
+        if column<4{
+            x = CGFloat(-1*(column-2))*SquareWidth-30
+        }else{
+            x = CGFloat((column-2))*SquareWidth
+        }
+        if row<4{
+            y = CGFloat(-1*(row-2))*SquareHeight
+        }else{
+            y = CGFloat((row-2))*SquareHeight+30
+        }
+        return CGPoint(x:x,y:y)
+    }
+    func convertPointOnBoard(point:CGPoint)->(row:Int,column:Int)?{
+        var x:Int
+        var y:Int
+        x=Int(point.x/75)
+        y=Int(point.y/75)
+        print(y,x)
+        if x == 0 && point.x<0{
+            x = 3
+        }else if point.x>0 && x == 0{
+            x = 4
+        }else{
+            x += 3
+        }
+        if x == -1{
+            x = 0
+        }else if x == 8{
+            x = 7
+        }
+        if y == 0 && point.y<0{
+            y = 3
+        }else if point.y>0 && y == 0{
+            y = 4
+        }else{
+            y += 3
+        }
+        if y == -1{
+            y = 0
+        }else if y == 8{
+            y = 7
+        }
+        print(point,x,y)
+        return (row:y,column:x)
     }
 }
